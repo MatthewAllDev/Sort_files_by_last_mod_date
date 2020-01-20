2d7e462a1e8062b436a0d8dad916f5adb784c8dc
MatthewAllDev <MatthewAllDev@users.noreply.github.com>
2020-01-20 13:56:29 +0300

    fix and feat
    1. Fix: problem with various registers in extensions
    2. Fix: problem with identical file names
    3. Implemented: deleting files with the same checksum (md5)
    4. Implemented: deleting the processed directory if it is empty

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Script to sort files by last modified date.
 # author Ilya Matthew Kuvarzin <luceo2011@yandex.ru>
-# version 1.0 dated January 17, 2020
+# version 1.1 dated January 20, 2020
 
 import os.path
 from datetime import datetime
+from hashlib import md5
 
 # SETTINGS
 recursively = True  # Sort files from subfolders?
+delete_duplicate_files = True  # Delete files with the same checksum?
+delete_empty_directory = True  # Delete processed directory if empty?
 input_directories = ['D:/FILES']  # List input directories. Example: ['/home/Images', '/home/Photos']
 output_directories = 'D:/FILES SORTED'  # Output folder
 files_extensions = ['.jpg', '.png']  # List file extensions. Example: ['.png', '.jpg', '.avi', '.txt']
@@ -14,12 +17,44 @@ files_extensions = ['.jpg', '.png']  # List file extensions. Example: ['.png', '
 
 def check_extension(file):
     for extension in files_extensions:
-        if file.endswith(extension):
+        if file.lower().endswith(extension.lower()):
             return True
 
 
+def get_checksum(file):
+    checksum = md5()
+    with open(file, 'rb') as f:
+        for chunk in iter(lambda: f.read(128* checksum.block_size), b''):
+            checksum.update(chunk)
+    f.close()
+    return checksum.hexdigest()
+
+
+def move_file(input_file, output_file):
+    try:
+        os.rename(input_file, output_file)
+    except FileExistsError:
+        if delete_duplicate_files:
+            if get_checksum(input_file) == get_checksum(output_file):
+                os.remove(input_file)
+                return
+        extension = output_file[output_file.lower().rfind('.'):]
+        file_name = output_file[:output_file.lower().rfind('.')]
+        move_file(input_file, '%s(1)%s' % (file_name, extension))
+    except OSError:
+        print('Move file %s failed' % input_file)
+        return
+
+
 def move_files(directory):
-    files = os.listdir(path=directory)
+    try:
+        files = os.listdir(directory)
+    except FileNotFoundError:
+        print('Directory %s not found' % directory)
+        return
+    except OSError:
+        print('Processing directory %s failed' % directory)
+        return
     for file in files:
         if os.path.isdir('%s/%s' % (directory, file)) and recursively:
             move_files('%s/%s' % (directory, file))
@@ -27,15 +62,23 @@ def move_files(directory):
         if check_extension(file):
             date = datetime.fromtimestamp(os.path.getmtime('%s/%s' % (directory, file)))
             folder = '%s/%i/%i/%i' % (output_directories, date.year, date.month, date.day)
+            input_file = '%s/%s' % (directory, file)
+            output_file = '%s/%s' % (folder, file)
             if os.path.exists(folder):
-                os.rename('%s/%s' % (directory, file), '%s/%s' % (folder, file))
+                move_file(input_file, output_file)
             else:
                 try:
                     os.makedirs(folder)
                 except OSError:
-                    print("Create directory %s failed" % folder)
-                    exit(0)
-                os.rename('%s/%s' % (directory, file), '%s/%s' % (folder, file))
+                    print('Create directory %s failed' % folder)
+                    return
+                move_file(input_file, output_file)
+    if (len(os.listdir(directory)) == 0) and delete_empty_directory:
+        try:
+            os.rmdir(directory)
+        except OSError:
+            print('Delete directory %s failed' % directory)
+            return
 
 
 for d in input_directories:
